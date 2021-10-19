48511a36860a5179a97aba1e9b685f37e85bba5b
Max-Felix Müller <32713448+MxFxM@users.noreply.github.com>
2021-10-19 23:29:46 +0200

    display bitmaps

--- ePaper_Teensy/src/main.cpp
+++ ePaper_Teensy/src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <Adafruit_I2CDevice.h>
+#include <GxEPD2_BW.h> // including both doesn't use more code or ram
+#include <GxEPD2_3C.h> // including both doesn't use more code or ram
+GxEPD2_BW<GxEPD2_154, GxEPD2_154::HEIGHT> display(GxEPD2_154(/*CS=D8*/ SS, /*DC=D3*/ 8, /*RST=D4*/ 9, /*BUSY=D2*/ 7));
 
-void setup() {
-  // put your setup code here, to run once:
+//#define MINIMAL_EXAMPLE
+#define BITMAP_EXAMPLE
+
+#ifdef BITMAP_EXAMPLE
+
+#include "200x200_mxfxmdragon_rot.h"
+
+
+void setup()
+{
+  display.init();
+  display.drawImage(bitmap, 0, 0, 200, 200, false, true, true);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){};
+#endif
+
+#ifdef MINIMAL_EXAMPLE
+void setup()
+{
+  display.init();
+  display.setTextColor(GxEPD_BLACK);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.print("Hello World!");
+  } while (display.nextPage());
 }
+
+void loop(){};
+#endif
